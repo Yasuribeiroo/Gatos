ba9a2098a6d070426f2e1b43cd4dd90c12e14154
Yasmim Riberiro ✩ <159492044+Yasuribeiroo@users.noreply.github.com>
2024-11-28 11:51:24 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+export default function App() {
+  return(
+    <div>
+      <h1 className='Title'>Vôce sabe tudo mesmo sobre os gatos?</h1>
+      <h2>Aqui 5 curiosidades sobre os bichanos!</h2>
+
+      <h3>1. Os gatos não saboreiam o gosto doce</h3>
+      <p>Além deles não conseguirem sentir o sabor doce, alimentos assim podem fazer mal para eles. Então, antes de dar qualquer "mimos" para seu pet, consulte primeiramente um veterinário.</p>
+
+      <h3>2. Direita ou Esquerda?</h3>
+      <p> Assim como nós, humanos, os gatinhos também tem mais facilidade usando uma de suas patas específicas sendo a direita ou esquerda. Gatinhas tendem a ser destra, já os gatinhos tendem a ser, em sua maioria, esquerdinos.</p>
+
+      <h3>3. Nem tudo é carinho</h3>
+      <p>Seu pet já se roçou alguma vez em vôce? Saiba que nem sempre é carinho! Eles também fazem isso para marca seu território. Suas glândulas emitem um odor voltadas a sua face. Sendo assim, não é ele que é seu, vôce que é dele!</p>
+
+      <h3>4. Nas alturas</h3>
+      <p>A prova de que esses animais têm 7 vidas está no fato de que alguns já sobreviveram a quedas de 20 metros. E isso aconteceu devido ao reflexo de endireitamento. Os seus olhos, os órgãos e ouvido interno, dizem ao gato onde ele está no espaço, para que ele consiga aterrar em pé. Mesmo os gatos sem cauda, responsável pelo equilíbrio, possuem esta capacidade.</p>
+
+      <h3>5. Felinos com pelagem branca e olhos azuis podem não escutar bem</h3>
+      <p>Existem gatos de pelagem branca e olhos azuis que ouvem normalmente, porém alguns gatos com esse padrão de características não. A veterinária explica que isso ocorre devido a alterações genéticas, ou seja, já nascem com uma alteração degenerativa na cóclea que leva à surdez. A alteração pode ocorrer somente em um dos ouvidos e o problema muitas vezes não é identificado</p>
+
+      <h3 className='last'>É sempre bom ter um amiguinho para fazer carinho, não é mesmo? Que tal um gatinho dessa vez?!</h3>
+
+      <img src='./Gato.jpg'></img>
+    </div>
+  )
+}
